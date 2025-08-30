09fb16b44043b65365808be95e9204a71e9f236e
Umanga Pathak <umangapathak999@gmail.com>
2025-08-30 11:27:11 +0545

    Updated create script

--- create.js
+++ create.js
@@ -4,22 +4,25 @@ import path from "path";
 
 const args = process.argv.slice(2);
 if (!args[0]) {
-  console.log("Usage: npx umangapathak999/electron-app <project-name>");
+  console.log("Usage: npx <repo> <project-name>");
   process.exit(1);
 }
 
 const projectName = args[0];
 const targetDir = path.join(process.cwd(), projectName);
-const templateDir = path.join(path.resolve(), "template");
+const templateDir = path.resolve(); // root of repo
 
 if (fs.existsSync(targetDir)) {
   console.log(`❌ Folder '${projectName}' already exists!`);
   process.exit(1);
 }
 
-// Copy template to new project folder
 fs.copySync(templateDir, targetDir, {
-  filter: (src) => !src.includes("node_modules") && !src.includes("dist") && !src.includes("dist-electron") && !src.includes(".git")
+  filter: (src) =>
+    !src.includes("node_modules") &&
+    !src.includes(".git") &&
+    !src.includes("dist") &&
+    !src.includes("dist-electron")
 });
 
 console.log(`✅ Project '${projectName}' created successfully!`);
